fb4561efbceb69de37b99cd98713b718ab614151
Manuel <manuel_f@manuel.local>
2025-11-01 21:32:19 -0300

    adding item to cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,12 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const localStorage = getLocalStorage("so-cart") || [];
+  localStorage.push(product)
+  setLocalStorage("so-cart", localStorage);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
