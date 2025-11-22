713583396d33b6fc214bb60005c3e5306c9ba1eb
KevinBezerra <kevinmbezerra@gmail.com>
2025-11-22 18:00:31 -0300

    Product List sort

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,21 +1,34 @@
-import ProductData from "./ProductData.mjs"
-import ProductList from "./ProductList.mjs"
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
 import { getParam } from "./utils.mjs";
 
-function initialize(){
-    const category =  getParam("category");
-    const listElement = document.querySelector(".product-list")
-    const dataSource = new ProductData(category)
-    const productList = new ProductList(category, dataSource, listElement)
-    productList.init()
-    renderTitle(category)
+function initialize() {
+  const category = getParam("category");
+  const listElement = document.querySelector(".product-list");
+  const dataSource = new ProductData(category);
+  
+  const productList = new ProductList(category, dataSource, listElement);
+  productList.init();
+  
+  renderTitle(category);
+
+  const sortSelect = document.querySelector("#sort-select");
+  if (sortSelect) {
+    sortSelect.addEventListener("change", (e) => {
+      const sortValue = e.target.value;
+      
+      productList.sortList(sortValue);
+    });
+  }
 }
 
-function renderTitle(category){
-  const productCategoryTitle = document.getElementById("top-products-category")
-  const firstCharacterUpper = category[0].toUpperCase()
-  const caseCategory = `${firstCharacterUpper}${category.substring(1, category.length).toLocaleLowerCase()}`
-  productCategoryTitle.innerText = `Top Products: ${caseCategory}`
+function renderTitle(category) {
+  const productCategoryTitle = document.getElementById("top-products-category");
+  if (category) {
+      const firstCharacterUpper = category[0].toUpperCase();
+      const caseCategory = `${firstCharacterUpper}${category.substring(1).toLowerCase()}`;
+      productCategoryTitle.innerText = `Top Products: ${caseCategory}`;
+  }
 }
 
-initialize()
+initialize();
