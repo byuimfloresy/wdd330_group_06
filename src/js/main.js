ac93a7803615873dfc318f97706dc891a58939ee
Manuel <manuel_f@manuel.local>
2025-12-12 16:07:47 -0300

    test

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,15 @@
 import { updateCartCount } from "./utils.mjs";
 
+
+async function test(){
+const resp = await fetch("https://catalog.data.gov/api/3/action/package_search",)
+        if (resp.ok){
+            console.log("success")
+        } else{
+            throw { name: "API Error", error: resp}
+        }
+        const data = await res.json()
+        return data.Result
+}
+test()
 updateCartCount();
